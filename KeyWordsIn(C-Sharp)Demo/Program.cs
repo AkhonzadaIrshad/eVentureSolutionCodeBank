8ae9b7b7309a99ad1366e4cb2d0075c778336ac4
Irshad Ahmed <Irshadahmed31@outlook.com>
2017-05-13 05:51:53 +0500

    Keywords and partial classes in C#

---
+++ KeyWordsIn(C-Sharp)Demo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyWordsIn_C_Sharp_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating object 
+            //Partial classes object treat as single object 
+            @class @class=new @class();
+            //Setting Value
+            @class.@string = "This is string";
+            @class.@int = 123;
+            @class.@float = 0.00f;
+            //Getting Value
+            Console.WriteLine(@class.@string);
+            Console.WriteLine(@class.@int);
+            Console.WriteLine(@class.@float);
+            Console.ReadKey();
+        }
+    }
+
+    //using same name of class as the same class identifier
+    //using partial keyword to create two classes with the same name 
+    public partial class @class
+    {
+        //using same of property name as the string identifier 
+        public string @string { get; set; }
+
+        public int @int { get; set; }
+    }
+
+    public partial class @class
+    {
+        public float @float { get; set; }
+    }
+}
